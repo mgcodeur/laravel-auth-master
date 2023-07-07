41700d4a0961dea598950b0270e6b2d89c509bba
mgcodeur <mgcodeur@gmail.com>
2023-07-07 10:24:20 +0300

    Put the user online when creating the user along with last_connection date.

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Mgcodeur\AuthMaster\AuthMaster;
 
-Route::get('totoss', function() {
+Route::get('totoss', function () {
     $user = AuthMaster::createUserAndGenerateAccessToken([
-        "first_name" => "Anais",
-        "last_name" => "Dubois",
-        "email" => "anaisdubois2@gmail.com",
-        "password" => bcrypt('password')
+        'first_name' => 'Anais',
+        'last_name' => 'Dubois',
+        'email' => 'anaisdubois2@gmail.com',
+        'password' => bcrypt('password'),
     ]);
 
     return response()->json($user);
